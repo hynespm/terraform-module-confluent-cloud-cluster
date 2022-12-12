db83d6c7bf4f88e999d5460461c13d19ce0d37e4
Patrick Hynes <phynes@confluent.io>
2022-12-12 21:48:29 +0100

    #1 : Update of provider

--- examples/main.tf
+++ examples/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     confluent = {
-      source  = "confluentinc/confluent"
-      version = "1.16.0"
+      source = "confluentinc/confluent"
+      version = "1.21.0"
     }
   }
   backend "local" {}
